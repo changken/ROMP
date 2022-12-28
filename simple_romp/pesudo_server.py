3512cbe0269d23980114d4bcc295dcfe227a9789
changken <pp24513@gmail.com>
2022-12-28 11:03:15 +0800

    add socket protocol

---
+++ simple_romp/pesudo_server.py
@@ -0,0 +1,27 @@
+import socket
+
+# socket config
+HOST = '127.0.0.1'
+PORT = 8000
+server_addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(server_addr)
+
+print('server start at: %s:%s' % (HOST, PORT))
+print('wait for connection...')
+
+def main():
+    global s
+
+    while True:
+        # 54 * 3 * 11 = 1944 => 有可能會超過3400
+        indata, addr = s.recvfrom(4096)
+        print('recvfrom ' + str(addr) + ': ' + indata.decode())
+
+        #outdata = 'echo ' + indata.decode()
+        outdata = 'recevied data!'
+        s.sendto(outdata.encode(), addr)
+
+if __name__ == "__main__":
+    main()
--- simple_romp/realtime_main.py
+++ simple_romp/realtime_main.py
@@ -1,10 +1,29 @@
 import bev
 from romp.utils import WebcamVideoStream
 import cv2
+import numpy as np
+import socket
+
+# socket config
+HOST = '127.0.0.1'
+PORT = 8000
+server_addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def handle_joint_points(joint_points):
     smpl_joints_line = ",".join([str(v) for v in joint_points[:54].flatten()])
-    print(smpl_joints_line)
+    #print(smpl_joints_line)
+    return smpl_joints_line
+
+def send_joint_points(joint_points):
+    global s
+    outdata = handle_joint_points(joint_points)
+    print('sendto ' + str(server_addr) + ': ' + outdata)
+    s.sendto(outdata.encode(), server_addr)
+
+    indata, addr = s.recvfrom(1024)
+    print('recvfrom ' + str(addr) + ': ' + indata.decode())
 
 
 def main():
@@ -23,8 +42,8 @@ def main():
             outputs = bev_model(frame)
             if outputs != None:
                 #print(outputs['joints'])
-                joint_points = outputs['joints']
-                handle_joint_points(joint_points)
+                joint_points = outputs['joints'].astype(np.float16)
+                send_joint_points(joint_points)
             else:
                 print('No person detected')
 
