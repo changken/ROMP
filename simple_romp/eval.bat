a7b39ef9d58467162db5e17bd60a36c4f6a640da
changken <pp24513@gmail.com>
2023-04-07 16:35:50 +0800

    加上註解

---
+++ simple_romp/eval.bat
@@ -0,0 +1,3 @@
+::romp --mode=video --calc_smpl --render_mesh -i=/path/to/video.mp4 -o=/path/to/output/folder/results.mp4 --save_video
+bev -m video -i D:\20230209\CAM1\C0005.mp4 -o ..\output\CAM1\results.mp4 --save_video
+bev -m video -i D:\20230209\CAM2\C0005.mp4 -o ..\output\CAM2\results.mp4 --save_video
