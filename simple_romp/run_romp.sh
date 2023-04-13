db253d2cb2cfcee84119b1eb36571f339876f298
andy091045 <andy091045@gmail.com>
2023-04-13 10:08:59 +0800

    保存一些腳本

---
+++ simple_romp/run_romp.sh
@@ -0,0 +1,6 @@
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_00_m.mp4 -o=../output/hd_00_00_romp/hd_00_00_result_romp.mp4 --save_video --GPU 0
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_01_m.mp4 -o=../output/hd_00_01_romp/hd_00_01_result_romp.mp4 --save_video --GPU 0
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_02_m.mp4 -o=../output/hd_00_02_romp/hd_00_02_result_romp.mp4 --save_video --GPU 0
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_03_m.mp4 -o=../output/hd_00_03_romp/hd_00_03_result_romp.mp4 --save_video --GPU 0
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_04_m.mp4 -o=../output/hd_00_04_romp/hd_00_04_result_romp.mp4 --save_video --GPU 0
+romp --mode=video --calc_smpl --render_mesh -i=/mnt/d/110598028/nick_mp4/hd_00_05_m.mp4 -o=../output/hd_00_05_romp/hd_00_05_result_romp.mp4 --save_video --GPU 0
