01c8baa9a09159d8c273aadadb84f06a020dedbd
changken <pp24513@gmail.com>
2023-05-26 11:33:20 +0800

    script

--- simple_romp/run_interview_2.bat
+++ simple_romp/run_interview_2.bat
@@ -10,4 +10,4 @@
 ::python -m romp.main --mode=video --calc_smpl --render_mesh -sc 0.05 --show_items=mesh -i=D:\110598028\input_video_dataset\str_network_short.mp4 -o=../output/str_network_short3/str_network_short3.mp4 --save_video --GPU 0
 
 ::python -m romp.main --mode=video --calc_smpl --render_mesh -sc 0.2 --temporal_optimize --show_items=mesh -i=D:\110598028\input_video_dataset\three_person_refine.mp4 -o=../output/three_person_refine/three_person_refine_result.mp4 --save_video --GPU 0
-python -m bev.main -m video --calc_smpl --temporal_optimize --smooth_coeff 0.2  -i=D:\110598028\input_video_dataset\three_person_refine.mp4 -o=../output/three_person_refine_bev/three_person_refine_bev_result.mp4 --save_video --GPU 0
+python -m romp.main --mode=webcam --calc_smpl --temporal_optimize --smooth_coeff 0.2 --show --show_items=mesh  --GPU 0
