6e1fef81403f906425961af2ab82460dc806cf56
changken <pp24513@gmail.com>
2023-07-07 21:24:40 +0800

    add

--- simple_romp/onnx.bat
+++ simple_romp/onnx.bat
@@ -1,8 +1,14 @@
 @REM python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/nick_japanese_more.mp4 -o=D:/110598028/ROMP/output/nick/results.mp4 --save_video -t -sc=3. --onnx
+@REM 少了 --show_items=tracking,mesh
+@REM python -m romp.main --mode=video --calc_smpl --render_mesh -i=D:/110598028/input_video_dataset/run_away/nick_japanese_more.mp4 -o=D:/110598028/ROMP/output/nick/results.mp4 --save_video -t -sc=3.
+
 @REM python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/boss_imb_more.mp4 -o=D:/110598028/ROMP/output/imb_boss_more/results.mp4 --save_video -t -sc=3. --onnx
-python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/president_weihan_more.mp4 -o=D:/110598028/ROMP/output/president_weihan_more/results.mp4 --save_video -t -sc=3. --onnx
-python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/str_network_short.mp4 -o=D:/110598028/ROMP/output/str_network_short/results.mp4 --save_video -t -sc=3. --onnx
+@REM python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/president_weihan_more.mp4 -o=D:/110598028/ROMP/output/president_weihan_more/results.mp4 --save_video -t -sc=3. --onnx
+@REM python -m romp.main --mode=video --calc_smpl --render_mesh --show_items=tracking,mesh -i=D:/110598028/input_video_dataset/run_away/str_network_short.mp4 -o=D:/110598028/ROMP/output/str_network_short/results.mp4 --save_video -t -sc=3. --onnx
+
 @REM python -m bev.main --mode video -i D:/110598028/input_video_dataset/run_away/nick_japanese_more.mp4 -o D:/110598028/ROMP/output/nick_BEV/BEV_results.mp4 --save_video --show_items=tracking,pj2d -t -sc=3.
+@REM  少了 --show_items=tracking,pj2d
+python -m bev.main --mode video -i D:/110598028/input_video_dataset/run_away/nick_japanese_more.mp4 -o D:/110598028/ROMP/output/nick_BEV/BEV_results.mp4 --save_video -t -sc=3.
 
 @REM 有視覺化 onnx
 @REM python -m romp.main --mode webcam --onnx --calc_smpl --render_mesh -t -sc=3. --show --show_items=tracking,mesh
